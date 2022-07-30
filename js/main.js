7480a881f1c5c73c3cd4305304c98f11bc5bbefd
m0o0n <strikunvlad@gmail.com>
2022-07-30 11:59:57 +0300

    add media.css add main.js changes in sidbar and messages header

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById('media_menu').onclick = ()=>{
+    document.getElementById('sidebar').style.display ="flex"
+}
+
+document.getElementById('sidebar__exit').onclick =()=>{
+    document.getElementById('sidebar').style.display ="none"
+}
